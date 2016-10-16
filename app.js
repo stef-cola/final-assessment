7e099b666e8d74122274706fad42ad086e5648c7
stefdotninja <stefania.cola1@gmail.com>
2016-10-16 17:35:20 +1100

    Broke content into templates and added routing

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-angular.module('abccars', ['ui.bootstrap']);
+var app = angular.module('abccars', ['ui.bootstrap','ngRoute']);
+app.config(function($routeProvider){
+  $routeProvider
+  .when("/", {
+    templateUrl : "templates/main.html"
+  })
+  .when("/signup", {
+    templateUrl : "templates/signup.html"
+  })
+  .when("/about", {
+    templateUrl : "templates/about.html"
+  })
+  .when("/contact", {
+    templateUrl : "templates/contact.html"
+  })
+  .when("/login", {
+    templateUrl : "templates/login.html"
+  })
+})
