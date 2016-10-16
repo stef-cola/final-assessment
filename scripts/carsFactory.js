e09b7e0431000c4b1c7dcd1b9d7f514837790822
stefdotninja <stefania.cola1@gmail.com>
2016-10-16 17:36:20 +1100

    Simplified the file to include shorthand for methods

--- scripts/carsFactory.js
+++ scripts/carsFactory.js
@@ -1,12 +1,9 @@
 angular
 	.module('abccars')
 	.factory('carsFactory', function($http) {
-
-		function getcars() {
-			return $http.get('data/data.json');
-		}
-
 		return {
-			getcars: getcars
+			getcars() {
+				return $http.get('cars.php');
+			}
 		}
 	});
